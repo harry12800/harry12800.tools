83036bbc5c5f1637dd3ed4dc875ef35ba0cba17d
wshua <sling@wshua.cc>
2018-11-20 11:08:05 +0800

    1

--- src/main/java/cn/harry12800/tools/Maps.java
+++ src/main/java/cn/harry12800/tools/Maps.java
@@ -2,6 +2,8 @@ package cn.harry12800.tools;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class Maps {
 	public static <K, V> HashMap<K, V> newHashMap() {
@@ -11,4 +13,10 @@ public class Maps {
 	public static <K, V> HashMap<K, V> newLinkedHashMap() {
 		return new LinkedHashMap<K, V>();
 	}
+	/**
+     * Creates a new fastest {@link ConcurrentMap} implementation for the current platform.
+     */
+    public static <K, V> ConcurrentMap<K, V> newConcurrentHashMap() {
+        return new ConcurrentHashMap<K, V>();
+    }
 }
